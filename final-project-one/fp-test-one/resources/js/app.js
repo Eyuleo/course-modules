7245423fb9582149d6981ced9f19dc5747aa7caa
Eyuleo <eyuleo10@gmail.com>
2025-10-28 18:44:12 +0300

    delete some files

--- final-project-one/fp-test-one/resources/js/app.js
+++
@@ -1,6 +0,0 @@
-import "./bootstrap";
-
-// Lightweight Alpine.js for small interactivity (menus, modals, toasts)
-import Alpine from "alpinejs";
-window.Alpine = Alpine;
-Alpine.start();
--- final-project-one/fp-test-one/resources/views/components/card.blade.php
+++
@@ -1,3 +0,0 @@
-<div {{ $attributes->merge(['class' => 'rounded-sm border border-[#e3e3e0] bg-white shadow-[0px_0px_1px_0px_rgba(0,0,0,0.03),0px_1px_2px_0px_rgba(0,0,0,0.06)]']) }}>
-    {{ $slot }}
-</div>
